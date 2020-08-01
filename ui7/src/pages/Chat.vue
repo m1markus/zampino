c273dc0372d09c323ed33cfb415c06bd23602c1a
mmm <mmm7@bluewin.ch>
2020-08-01 15:57:03 +0200

    QA Add some Framework7 Views

---
+++ ui7/src/pages/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Main view-->
+  <f7-view main>
+    <f7-page>
+      <f7-navbar title="Chat"></f7-navbar>
+      <!-- Page content -->
+      <f7-block>
+        <p>Here comes main view page text</p>
+      </f7-block>
+      <!-- Buttons to open panels -->
+      <f7-row>
+        <f7-col>
+          <f7-button panel-open="left">Left Panel</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-page>
+  </f7-view>
+</template>
