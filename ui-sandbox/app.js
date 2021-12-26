d9442de9059ea9efbf04403fffcb2a30981d9f86
mmm <mmm7@bluewin.ch>
2021-12-26 16:50:33 +0100

    Add html sandbox dir

---
+++ ui-sandbox/app.js
@@ -0,0 +1,13 @@
+console.log("this is my app.js output")
+
+function myFunc1() {
+    console.log('func myFunc1 called return 17');
+    return 17;
+}
+
+var a = myFunc1();
+console.log('a initialized with: ' + a)
+
+console.log(myFunc1)
+
+//alert('this is loded')
